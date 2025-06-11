1c7a9615c68c89f2b7b07b7f4fcb700a30f9b133
p7666 <priyankasonawane7666@gmail.com>
2025-06-11 20:01:06 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,20 @@
+//array
+
+const myArr = [0,1,2,3,4,5]
+console.log(myArr[0]);
+
+const myArr2=new Array(1,2,3,4,5)
+
+//array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+
+console.log(myArr.includes(9))
+console.log(myArr.indexOf(9))
+console.log(myArr)
