8124b0355a89c2509080f504c26d5272f3b09f81
p7666 <priyankasonawane7666@gmail.com>
2025-06-11 19:44:33 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,20 @@
+//Dates
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleTimeString())
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2025 , 5 , 12)
+// let myCreatedDate = new Date(2025 , 5 , 12 , 5, 3)
+// let myCreatedDate = new Date("2025-06-12")
+let myCreatedDate = new Date("06-12-2025")
+// console.log(myCreatedDate.toLocaleTimeString())
+// console.log(myCreatedDate.toLocaleDateString())
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime());
